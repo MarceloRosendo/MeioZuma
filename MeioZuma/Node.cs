66a2e3997a2fc32c0a4ae7ef7e01c7fca4894027
MarceloRosendo <marcelomax159@gmail.com>
2020-05-13 21:41:48 -0300

    Primeiro commit

---
+++ MeioZuma/Node.cs
@@ -0,0 +1,15 @@
+namespace MeioZuma
+{
+    public class Node
+    {
+        public int Valor;
+    
+        public Node Prox;
+        public Node Anter;
+    
+        public Node(int d)
+        {
+            Valor = d;
+        }
+    }
+}
---
+++ MeioZuma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace MeioZuma
+{
+    internal class Program
+    {
+        private static String presentation; 
+        
+        public static void Main(string[] args)
+        {
+            String file = File.ReadAllText("home.txt");
+            Console.WriteLine(file);
+        }
+
+        private void gameloop()
+        {
+            
+        }
+        
+    }
+}
